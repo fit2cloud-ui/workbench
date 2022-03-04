5b2d44de96c7958c4a57042479796a8faa30ace0
wangdan <dan.wang@fit2cloud.com>
2022-03-04 17:54:15 +0800

    feat: 添加 docs

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-
 module.exports = {
+  publicPath: "./",
   pages: {
     index: {
-      entry: 'examples/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
+      entry: "./examples/main.js",
+      template: "./public/index.html",
+      filename: "index.html",
+      title: 'Workbench',
     }
   }
 }
